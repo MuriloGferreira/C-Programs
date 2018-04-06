c68ba1da87a56e38485cee8d22754b3f58cea2a2
iHeroSoftware <38138777+iHeroSoftware@users.noreply.github.com>
2018-04-06 14:47:23 -0300

    Update exercicio1.c

--- exercicio1.c
+++ exercicio1.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{ int i, soma=0;
+	for(i=10;i<=200;i++)
+	{
+	if(i%2==0)
+	{printf("%d\n",i);
+	}
+	else
+	{
+		soma+=i;}
+	}
+	printf("%d", soma);
+		return 0;
+}
 
